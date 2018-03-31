4b84e651209795913ec641e3ce75b4dcdbbf70da
SonJJ <zzzzax@naver.com>
2018-03-31 18:03:06 +0900

    에러수정

--- usrdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ usrdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -5,21 +5,32 @@ import java.sql.*;
 
 public class UserDao {
     public User get(int id) throws SQLException, ClassNotFoundException{
+        //사용자는 어디`에 저장되어있나?
+        //Database를 사용해보자, Mysql을 사용해보자
         Class.forName("com.mysql.jdbc.Driver");
+
+        //Connection을 맺고
         Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/jeju",
+                "jdbc:mysql://localhost/jeju?characterEncoding=utf-8",
                 "root",
-                "root1234");
+                "root12345");
+
+        //쿼리를 만들어서
         PreparedStatement preparedStatement = connection.prepareStatement(
-                "SELECT * FROM userinfo WHER id=?");
-        //결과 맵핑
+                "SELECT * FROM userinfo WHERE i~d=?");
+        preparedStatement.setInt(1,id);
+
+        //실행시키고
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
+
+        //결과를 User에 잘 매핑하
         User user = new User();
         user.setId(resultSet.getInt("id"));
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
 
+        //자원을 해지한다.
         resultSet.close();
         preparedStatement.close();
         connection.close();
--- usrdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ usrdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -9,13 +9,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDaoTest {
     @Test
-
     public void get() throws SQLException, ClassNotFoundException {
         int id = 1;
         UserDao userDao = new UserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(1));
-        assertThat(user.getName(), is("손경호"));
+        assertThat(user.getName(), is("허윤호"));
         assertThat(user.getPassword(), is("1234"));
     }
+
+    
 }
