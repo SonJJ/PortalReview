b41edc181e0fdb2dd909670b22b26bc7d115238c
SonJJ <zzzzax@naver.com>
2018-04-19 17:44:49 +0900

    일단 푸쉬

---
+++ usrdao/src/main/java/kr/ac/jejunu/JejuConnectionMaker.java
@@ -0,0 +1,17 @@
+package kr.ac.jejunu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JejuConnectionMaker implements ConnectionMaker {
+
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/jeju?characterEncoding=utf-8",
+                "root",
+                "root12345");
+    }
+}
